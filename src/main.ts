c4da53229c51572b165ae7d03c359b9fd4978866
liangpeng0126 <l17321449274@163.com>
2022-08-08 22:39:27 +0800

    feat: init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+
+import antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import './style.css'
+
+import App from './App.vue'
+
+createApp(App).use(antd).mount('#app')
