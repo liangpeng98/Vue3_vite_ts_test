3378271d3efdac26c70a42f30b6feb333b41b390
liangpeng0126 <l17321449274@163.com>
2022-08-09 22:22:08 +0800

    feat: add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname,'./src'),
+      'com': path.resolve(__dirname,'./src/components')
+    }
+  },
+  server: {
+    port: 8080,
+    open: true,    
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
